d878b15f068bab9240964eba87b7473468a97701
SeniorDmitriy <dimaponomarenko443@gmail.com>
2022-01-08 13:43:37 +0200

    added section firts, info block

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,10 @@ $(function () {
   let mobileMenu = $('.mobile-menu');
   let headerFixedBlock = $('.header__action');
   let userLink = $('.header__info-personal');
-  let headerScrollTop = header.innerHeight();
+  let headerScrollTop = header.outerHeight();
   let headerContainer = $('.header__container');
-  // let firstBlockPadding = $('main div:first').css('padding-top');
-  // let navbarHeight = navbar.outerHeight();
-  // let paddingTop = navbarHeight + parseInt(firstBlockPadding.match(/\d+/));
+  let firstBlockPadding = $('main section:first').css('padding-top');
+  let paddingTop = headerScrollTop + parseInt(firstBlockPadding.match(/\d+/));
 
   if ($(window).width() <= 1200) {
     $('.header__info-link--marker').removeClass('header__info-link').prependTo('.header__action-items');
@@ -23,22 +22,28 @@ $(function () {
         userLink.appendTo(headerFixedBlock);
         navbarSubmenu.appendTo(mobileMenu);
         header.addClass('header--fixed');
-        // $('main div:first').css('padding-top', paddingTop);
+        $('main section:first').css('padding-top', paddingTop);
       } else {
         header.removeClass('header--fixed');
         navbarSubmenu.appendTo('.dropdown-item');
         menuNavbar.appendTo(headerContainer);
         userLink.appendTo('.header__info-user');
-        // navbar.removeClass('navbar--fixed');
-        // $('main div:first').css('padding-top', '');
+        $('main section:first').css('padding-top', '');
       }
     });
   }
+
 
   if ($(window).width() <= 992) {
     header.addClass('header--fixed');
     menuNavbar.prependTo(headerFixedBlock);
     navbarSubmenu.appendTo(mobileMenu);
+
+    headerScrollTop = header.innerHeight();
+    paddingTop = headerScrollTop + parseInt(firstBlockPadding.match(/\d+/));
+    $('main section:first').css('padding-top', paddingTop);
+
+    console.log(headerScrollTop);
 
     $('.submenu__item-text').on('click', function () {
       $(this).next().slideToggle();
